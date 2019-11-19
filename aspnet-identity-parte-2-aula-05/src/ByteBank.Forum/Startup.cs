a0209e3c6555847148d1b1da3def6dd69e0e58a7
Thyago Bezerra Lima <thyagobezerra8608@gmail.com>
2019-11-19 00:03:30 -0300

    aula 2

---
+++ aspnet-identity-parte-2-aula-05/src/ByteBank.Forum/Startup.cs
@@ -0,0 +1,99 @@
+﻿using ByteBank.Forum.App_Start.Identity;
+using ByteBank.Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(ByteBank.Forum.Startup))]
+
+namespace ByteBank.Forum
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() =>
+                new IdentityDbContext<UsuarioAplicacao>("DefaultConnection"));
+
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var dbContext = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(dbContext);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get<IUserStore<UsuarioAplicacao>>();
+                    var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                    var userValidator = new UserValidator<UsuarioAplicacao>(userManager);
+                    userValidator.RequireUniqueEmail = true;
+
+                    userManager.UserValidator = userValidator;
+                    userManager.PasswordValidator = new SenhaValidador()
+                    {
+                        TamanhoRequerido = 6,
+                        ObrigatorioCaracteresEspeciais = true,
+                        ObrigatorioDigitos = true,
+                        ObrigatorioLowerCase = true,
+                        ObrigatorioUpperCase = true
+                    };
+
+                    userManager.EmailService = new EmailServico();
+
+                    var dataProtectionProvider = opcoes.DataProtectionProvider;
+                    var dataProtectionProviderCreated = dataProtectionProvider.Create("ByteBank.Forum");
+
+                    userManager.UserTokenProvider = new DataProtectorTokenProvider<UsuarioAplicacao>(dataProtectionProviderCreated);
+
+                    userManager.MaxFailedAccessAttemptsBeforeLockout = 3;
+                    userManager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    userManager.UserLockoutEnabledByDefault = true;
+
+                    return userManager;
+                });
+
+            builder.CreatePerOwinContext<SignInManager<UsuarioAplicacao, string>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userManager = contextoOwin.Get<UserManager<UsuarioAplicacao>>();
+                    var signInManager =
+                        new SignInManager<UsuarioAplicacao, string>(
+                            userManager,
+                            contextoOwin.Authentication);
+
+                    return signInManager;
+                });
+
+            builder.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+
+            builder.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ExternalCookie
+            });
+
+            builder.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions{
+                ClientId = ConfigurationManager.AppSettings["google:client_id"],
+                ClientSecret = ConfigurationManager.AppSettings["google:client_secret"],
+                Caption = "Google"
+
+            });
+
+               
+        }
+    }
+}
